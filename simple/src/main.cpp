a756d729528e4d10f3f92f3130f8d4a00811d48c
Agus Yudiana <myheavensent@gmail.com>
2020-07-02 20:21:41 +0700

    update code in main

--- simple/src/main.cpp
+++ simple/src/main.cpp
@@ -11,3 +11,29 @@ int main()
     cout << "You entered " << number <<"\n";    
     return 0;
 }
+
+/*#include "../include/helloworld.h"
+#include <gtkmm/application.h>
+
+int main (int argc, char *argv[])
+{  auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+
+  HelloWorld helloworld;
+
+  Shows the window and returns when it is closed.
+  return app->run(helloworld);/
+}*/
+
+/*#include <gtkmm.h>
+
+int main(int argc, char *argv[])
+{
+  auto app =
+    Gtk::Application::create(argc, argv,
+      "org.gtkmm.examples.base");
+
+  Gtk::Window window;
+  window.set_default_size(200, 200);
+
+  return app->run(window);
+}*/
